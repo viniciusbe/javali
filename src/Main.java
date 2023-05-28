78631c05fb40c4d341e69dc85ba50acfb12c6f4f
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-05-28 00:30:47 -0300

    Date

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,55 @@ import model.exceptions.DomainException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 //        exceptions();
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+
+        // current date-time
+        LocalDate localDate = LocalDate.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Instant instant = Instant.now(); // GMT timezone
+
+        // parsing from ISO string
+        LocalDate localDate1 = LocalDate.parse("2022-07-20");
+        LocalDateTime localDateTime1 = LocalDateTime.parse("2022-07-20T01:30:23");
+        Instant instant1 = Instant.parse("2022-07-20T01:30:23Z");
+        Instant instant2 = Instant.parse("2022-07-20T01:30:23-03:00");
+
+        // parsing from custom format
+        LocalDate localDate2 = LocalDate.parse("20/07/2022", fmt1);
+        LocalDateTime localDateTime2 = LocalDateTime.parse("20/07/2022 01:30",fmt2);
+
+        // from year, month and day
+        LocalDate localDate3 = LocalDate.of(2022, 7, 20);
+        LocalDateTime localDateTime3 = LocalDateTime.of(2022, 7, 20,1,30);
+
+        // format to string
+        String localDateToFmt1 = localDate3.format(fmt1);
+        String localDateTimeToFmt2 = localDateTime3.format(fmt2);
+        String instantToFmt3 = fmt3.format(instant1);
+
+        // convert instant to LocalDate
+        LocalDate instantToLocalDate = LocalDate.ofInstant(instant1, ZoneId.of("Portugal"));
+        LocalDateTime instantToLocalDateTime = LocalDateTime.ofInstant(instant1, ZoneId.of("Portugal"));
+
+        int dayOfMonth = instantToLocalDate.getDayOfMonth();
+        int month = instantToLocalDate.getMonthValue();
+        int year = instantToLocalDate.getYear();
+        System.out.println(year);
+
     }
 
     public static void exceptions() {
